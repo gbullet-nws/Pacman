74a5a66217f04de66ea4282be46d064260a2af9a
Guillaume BULLET <gbullet@normandiewebschool.fr>
2021-01-13 16:12:02 +0100

    HUD en HTML

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 // var du canvas
-var myCanvas, myCanvasContext,canvasWidth=1920,canvasHeight= 1080;
+//var myCanvas, myCanvasContext,canvasWidth=1920,canvasHeight= 1080;
 // var de pacman
-var pacman;
+//var pacman;
 //var fantômes
-var fantome1, fantome2, fantome3, fantome4;
+//var fantome1, fantome2, fantome3, fantome4;
 
-var Labyrinthe0 = [
-                    [ 9, 1, 1, 1, 1 ],
-                    [ 8, 0, 0, 0, 2 ],
-                    [ 8, 0, 0, 0, 2 ],
-                    [ 8, 0, 0, 0, 2 ],
-                    [ 12, 4, 4, 4, 6 ],
-                  ];
+//var Labyrinthe0 = [
+//                    [ 9, 1, 1, 1, 1 ],
+//                    [ 8, 0, 0, 0, 2 ],
+//                    [ 8, 0, 0, 0, 2 ],
+//                    [ 8, 0, 0, 0, 2 ],
+//                    [ 12, 4, 4, 4, 6 ],
+//                  ];
 
 //for (var largeur = 0; colonne < hauteur; colonne++)
 //{
