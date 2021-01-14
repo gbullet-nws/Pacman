c4e5072f7dad3cc168ffd7bda9777e7770aa5f46
Guillaume BULLET <gbullet@normandiewebschool.fr>
2021-01-14 01:17:16 +0100

    v0.0.1

--- main.js
+++ main.js
@@ -1,25 +1,43 @@
-// var du canvas
-//var myCanvas, myCanvasContext,canvasWidth=1920,canvasHeight= 1080;
-// var de pacman
-//var pacman;
-//var fantômes
-//var fantome1, fantome2, fantome3, fantome4;
+// le canvas
+var myCanvas, myCanvasContext,canvasWidth=1920,canvasHeight= 1080;
+// le pacman
+var pacman;
+// les fantômes
+var fantome1, fantome2, fantome3, fantome4;
 
-//var Labyrinthe0 = [
-//                    [ 9, 1, 1, 1, 1 ],
-//                    [ 8, 0, 0, 0, 2 ],
-//                    [ 8, 0, 0, 0, 2 ],
-//                    [ 8, 0, 0, 0, 2 ],
-//                    [ 12, 4, 4, 4, 6 ],
-//                  ];
+var Labyrinthe0 = [
+                    [ 9, 1, 1, 1, 1 ],
+                    [ 8, 0, 0, 0, 2 ],
+                    [ 8, 0, 0, 0, 2 ],
+                    [ 8, 0, 0, 0, 2 ],
+                    [ 12, 4, 4, 4, 6 ],
+                  ];
 
-//for (var largeur = 0; colonne < hauteur; colonne++)
-//{
-//    for (var colonne = 0; ligne < largeur; ligne++)
+for (var largeur = 0; colonne < hauteur; colonne++)
+{
+    for (var colonne = 0; ligne < largeur; ligne++)
 
-//    Tableau[Labyrinthe0[largeur][col]]
+    Tableau[Labyrinthe0[largeur][col]]
 
-//}
+}
+
+// ____________________________________________________ //
+// ____________________________________________________  //
+
+/*
+            for (var column = 0; column < map.columns; column++) 
+            {
+                for (var row = 0; row < map.rows; row++) 
+                {
+                    var tile = map.getTile(column, row);
+                    var x = column * map.tileSize;
+                    var y = row * map.tileSize;
+                    drawTile(tile, x, y);
+                }
+            }
+            */
+
+            //--- Initialisation de PacMan ---//
 
 
 
