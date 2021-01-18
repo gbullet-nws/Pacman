1b27accfab0c302899114ea2c605884571b9854d
Guillaume BULLET <gbullet@normandiewebschool.fr>
2021-01-18 09:57:09 +0100

    Maquette HTML / CSS / JavaScript
    version de démo

--- ressources.js
+++ ressources.js
@@ -1,5 +1,5 @@
 // le canvas
-var myCanvas, myCanvasContext,canvasWidth=1920,canvasHeight= 1080;
+var myCanvas, myCanvasContext, canvasWidth=1920, canvasHeight= 1080;
 // le pacman
 var pacman;
 // les fantômes
@@ -39,8 +39,44 @@ for (var largeur = 0; colonne < hauteur; colonne++)
 
             //--- Initialisation de PacMan ---//
 
+ /*
 
+<script type="text/javascript">
+ 
+ var canvas = document.getElementById("myCanvas");
+ var ctx = canvas.getContext("2d");
 
+ var mur1 = new Image();
+ mur1.onload = function() 
+ {
+     ctx.drawImage(mur1, 0, 0, 50, 50);
+ };
+ mur1.src = "assets/img/mur1.png";
+
+ var mur2 = new Image();
+ mur2.onload = function() 
+ {
+     ctx.drawImage(mur2, 100, 100, 50, 50);
+ };
+ mur2.src = "assets/img/mur2.png";
+
+ var mur3 = new Image();
+ mur3.onload = function() 
+ {
+     ctx.drawImage(mur3, 400, 400, 50, 50);
+ };
+ mur3.src = "assets/img/mur3.png";
+
+ var pacman = new Image();
+ pacman.onload = function() 
+ {
+     ctx.drawImage(pacman, 100, 100, 48, 48);
+ };
+ pacman.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Pacman.svg/220px-Pacman.svg.png";
+
+</script>
+
+/*
 
 
 
